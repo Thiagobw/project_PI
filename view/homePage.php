cbbb42c7156a8c84d48bc27e39a72d833d23e0bb
Thiagobw <thgleopoldo900@gmail.com>
2022-08-18 05:28:26 -0300

    banner home page

--- view/homePage.php
+++ view/homePage.php
@@ -19,6 +19,14 @@
     ?>
 </header>
 
+<div class="container-fluid mt-1">
+    <div class="row">
+        <div class="col-12 banner">
+            <img class="w-75" src="img/products/tenis 1.jpg" alt="">
+        </div>
+    </div>
+</div>
+
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
