e29025b15270a6a4891bb6e9a28706e5d42914f0
Thiagobw <thgleopoldo900@gmail.com>
2022-08-30 10:37:55 -0300

    form

--- view/js/formsValid.js
+++ view/js/formsValid.js
@@ -7,6 +7,10 @@ $('#form2').submit(function(e){
     var email = $('#email').val();
     var pass = $('#pass').val();
 
+    name.on('keyup', function() {
+        alert('digitando');
+    });
+    //fazer evento de validalção em tempo real
     if(name.length == 0 || cpf.length == 0 || tell.length == 0 || email.length == 0 || pass.length == 0){
         alert("preencha todos os campos para poder fazer cadastro!");
     } else{
