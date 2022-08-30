52369128595a7f331b0d880d3fe62f198540fd28
Thiagobw <thgleopoldo900@gmail.com>
2022-08-30 10:28:30 -0300

    form

--- view/js/formsValid.js
+++ view/js/formsValid.js
@@ -10,17 +10,20 @@ $('#form2').submit(function(e){
     if(name.length == 0 || cpf.length == 0 || tell.length == 0 || email.length == 0 || pass.length == 0){
         alert("preencha todos os campos para poder fazer cadastro!");
     } else{
-
         alert("tudo certo!");
         
-        $ajax({
-            URL: '',
-            method: '',
-            data: {name: name, cpf: cpf, tell: tell, email: email, pass: pass},
-            dataType: 'json'
-        }).done(function(result){
-            
-        })
+        
     }
 })
- 
+
+
+ function ajax() {
+    $ajax({
+        URL: '',
+        method: '',
+        data: {name: name, cpf: cpf, tell: tell, email: email, pass: pass},
+        dataType: 'json'
+    }).done(function(result){
+        
+    })
+ }
