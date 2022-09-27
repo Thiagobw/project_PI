d32aade7c40cf4f60d992d1612687c6093e6db64
Thiagobw <thgleopoldo900@gmail.com>
2022-09-27 04:29:57 -0300

    nd

--- view/dashboard/customers.php
+++ view/dashboard/customers.php
@@ -167,7 +167,6 @@
             </nav>
             <!-- Navbar End -->
 
-
             <!-- Table Start -->
             <div class="container-fluid pt-4 px-4">
                 <div class="row g-4">
