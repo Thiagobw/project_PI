a0350c8fd6c1d453e7f0dbb7681a11a45d33294e
thiagobw <thgleopoldo900@gmail.com>
2022-09-27 09:36:13 -0300

    class produto com agregacao

--- model/Products.php
+++ model/Products.php
@@ -1,7 +1,23 @@
 <?php
+require_once 'Model.php';
 
 class Products {
     private $name;
     private $amount;
-    
+    private $modelo; //fazer agregação
+
+    public function setName($name) {
+        return $this->$name = $name;
+    }
+    public function getName() {
+        return $this->$name;
+    }
+
+    public function setAmount($amount) {
+        return $this->$amount = $amount;
+    }
+    public function getAmount() {
+        return $this->$amount;
+    }
+
 }
