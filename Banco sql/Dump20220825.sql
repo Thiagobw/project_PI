be89b0c1160e7f0f66158235d5724dedab70665f
Thiagobw <thgleopoldo900@gmail.com>
2022-09-30 01:13:52 -0300

    db

--- Banco sql/Dump20220825.sql
+++ Banco sql/Dump20220825.sql
@@ -17,10 +17,11 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`mydb` /*!40100 DEFAULT CHARACTER SET ut
 USE `mydb`;
 
 -- Dump completed on 2022-08-25 11:39:03
-DROP TABLE IF EXISTS `usuarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 /*Table structure for table `usuarios` */
+DROP TABLE IF EXISTS `usuarios`;
+
 CREATE TABLE `usuarios` (
 
   `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
@@ -36,11 +37,12 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Table structure for table `caracteristicas` */
 
+/*Table structure for table `caracteristicas` */
 DROP TABLE IF EXISTS `caracteristicas`;
 
 CREATE TABLE `caracteristicas` (
+
   `idCaracteristicas` int(11) NOT NULL AUTO_INCREMENT,
   `fk_produtos_id_produtos` int(11) NOT NULL,
   `solado` varchar(20) NOT NULL,
@@ -51,30 +53,30 @@ CREATE TABLE `caracteristicas` (
   `entresola` varchar(20) NOT NULL,
   PRIMARY KEY (`idCaracteristicas`),
   UNIQUE KEY `idCaracteristicas_UNIQUE` (`idCaracteristicas`)
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `caracteristicas` */
 
 /*Table structure for table `cliente` */
-
 DROP TABLE IF EXISTS `cliente`;
 
 CREATE TABLE `cliente` (
+
   `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
   `CPF` char(14) NOT NULL,
   PRIMARY KEY (`id_cliente`),
   UNIQUE KEY `CPF_UNIQUE` (`CPF`),
   UNIQUE KEY `id_cliente_UNIQUE` (`id_cliente`)
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `cliente` */
 
 /*Table structure for table `email` */
-
 DROP TABLE IF EXISTS `email`;
 
 CREATE TABLE `email` (
+
   `id_email` int(11) NOT NULL AUTO_INCREMENT COMMENT `\n\n`,
   `email` varchar(60) NOT NULL DEFAULT `nao informado`,
   `Vendedor_id_vendedor` int(11) NOT NULL,
@@ -90,15 +92,15 @@ CREATE TABLE `email` (
   CONSTRAINT `fk_Email_Vendedor` FOREIGN KEY (`Vendedor_id_vendedor`) REFERENCES `vendedor` (`id_vendedor`) ON DELETE NO ACTION ON UPDATE NO ACTION,
     KEY `fk_usuario_Cliente1_idx` (`Cliente_id_usuario`),
   CONSTRAINT `fk_usuario_Cliente1` FOREIGN KEY (`Cliente_id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `email` */
 
 /*Table structure for table `endereco` */
-
 DROP TABLE IF EXISTS `endereco`;
 
 CREATE TABLE `endereco` (
+
   `id_endereco` int(11) NOT NULL AUTO_INCREMENT,
   `numero_endereco` int(11) DEFAULT NULL,
   `rua` varchar(45) DEFAULT NULL,
@@ -112,15 +114,15 @@ CREATE TABLE `endereco` (
   UNIQUE KEY `id_endereco_UNIQUE` (`id_endereco`),
   KEY `fk_Endereco_Cliente1_idx` (`Cliente_id_cliente`),
   CONSTRAINT `fk_Endereco_Cliente1` FOREIGN KEY (`Cliente_id_cliente`) REFERENCES `cliente` (`id_cliente`) ON DELETE NO ACTION ON UPDATE NO ACTION
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `endereco` */
 
 /*Table structure for table `pedido` */
-
 DROP TABLE IF EXISTS `pedido`;
 
 CREATE TABLE `pedido` (
+
   `id_pedido` int(11) NOT NULL AUTO_INCREMENT,
   `valor_pedido` varchar(20) NOT NULL,
   `forma_pagamento` varchar(10) NOT NULL,
@@ -132,15 +134,15 @@ CREATE TABLE `pedido` (
   KEY `fk_Pedido_Cliente1_idx` (`Cliente_id_cliente`),
   CONSTRAINT `fk_Pedido_Cliente1` FOREIGN KEY (`Cliente_id_cliente`) REFERENCES `cliente` (`id_cliente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_Pedido_Vendedor1` FOREIGN KEY (`Vendedor_id_vendedor`) REFERENCES `vendedor` (`id_vendedor`) ON DELETE NO ACTION ON UPDATE NO ACTION
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `pedido` */
 
 /*Table structure for table `pedido_produto` */
-
 DROP TABLE IF EXISTS `pedido_produto`;
 
 CREATE TABLE `pedido_produto` (
+
   `idPedido_Produto` int(11) NOT NULL AUTO_INCREMENT,
   `quantidade` int(11) NOT NULL,
   `valor` int(11) NOT NULL,
@@ -150,31 +152,34 @@ CREATE TABLE `pedido_produto` (
   KEY `fk_Pedido_Produto_Pedido1_idx` (`Pedido_id_pedido`),
   KEY `fk_Pedido_Produto_Produtos1_idx` (`Produtos_idProdutos`),
   CONSTRAINT `fk_Pedido_Produto_Produtos1` FOREIGN KEY (`Produtos_idProdutos`) REFERENCES `produtos` (`id_produtos`) ON DELETE NO ACTION ON UPDATE NO ACTION
+
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
-/*Table structure for table `produtos` */
 
+/*Table structure for table `produtos` */
 DROP TABLE IF EXISTS `produtos`;
 
 CREATE TABLE `produtos` (
+
   `id_produtos` int(11) NOT NULL AUTO_INCREMENT,
   `nome_produto` varchar(45) NOT NULL,
-  `preco_produto` varchar(20) DEFAULT NULL,
+  `preco_produto` int(11) DEFAULT NULL,
   `quantidade` int(11) NOT NULL,
   `Modelo_idModelo` int(11) NOT NULL,
   PRIMARY KEY (`id_produtos`),
   UNIQUE KEY `id_produtos_UNIQUE` (`id_produtos`),
   KEY `fk_Produtos_Modelo1_idx` (`Modelo_idModelo`)
+
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 
 /*Table structure for table `telefone` */
-
 DROP TABLE IF EXISTS `telefone`;
 
 CREATE TABLE `telefone` (
+
   `id_telefone` int(11) NOT NULL AUTO_INCREMENT,
-  `numero` varchar(18) NOT NULL,
+  `numero` char(15) NOT NULL,
   `Cliente_id_cliente` int(11) NOT NULL,
   `Vendedor_id_vendedor` int(11) NOT NULL,
   PRIMARY KEY (`id_telefone`),
@@ -184,14 +189,15 @@ CREATE TABLE `telefone` (
   KEY `fk_Telefone_Vendedor1_idx` (`Vendedor_id_vendedor`),
   CONSTRAINT `fk_Telefone_Cliente1` FOREIGN KEY (`Cliente_id_cliente`) REFERENCES `cliente` (`id_cliente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_Telefone_Vendedor1` FOREIGN KEY (`Vendedor_id_vendedor`) REFERENCES `vendedor` (`id_vendedor`) ON DELETE NO ACTION ON UPDATE NO ACTION
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 /*Table structure for table `vendedor` */
-
 DROP TABLE IF EXISTS `vendedor`;
 
 CREATE TABLE `vendedor` (
+
   `id_vendedor` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) DEFAULT NULL,
   `CPF` varchar(14) NOT NULL,
@@ -204,9 +210,10 @@ CREATE TABLE `vendedor` (
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+/*================================================*/
+/*  DATA  */
 
 /*Data for the table `usuarios`*/
-
 insert into `usuarios` (`id_usuario`, `nome`, `cpf`, `telefone`, `email`, `senha`) values
 (1, `Ferdinando Rainert`, `119.082.619-43`, `(47)920000706`, `fer.rainert@gmail.com`, ``), /* php_hash = 321456*/
 (2, `Bruno Erbs`, `108.912.169-52`, `(47)99164992`, `brunoerbs4@gmail.com`, ``), /* php_hash = erbs1311 */
@@ -214,13 +221,7 @@ insert into `usuarios` (`id_usuario`, `nome`, `cpf`, `telefone`, `email`, `senha
 (4, `Thiago Leopoldo`, `076.147.659-82`, `(49) 96850-6313`,`thgleopoldo901@gmail.com`, ``); /* pho_hash = 654321*/
 
 
-/*Data for the table `telefone` */
-
-
-
-
 /*Data for the table `produtos` */
-
 insert  into `produtos`(`id_produtos`,`nome_produto`,`preco_produto`,`quantidade`,`Modelo_idModelo`) values 
 (2, `Nike Air Jordan High 1 Tie Dye - unissex`, 400, 98, 0),
 (3, `Balenciaga Triple S`, 250, 18, 0),
@@ -229,7 +230,6 @@ insert  into `produtos`(`id_produtos`,`nome_produto`,`preco_produto`,`quantidade
 
 
 /*Data for the table `pedido_produto` */
-
 insert  into `pedido_produto`(`idPedido_Produto`,`quantidade`,`valor`,`Pedido_id_pedido`,`Produtos_idProdutos`) values 
 (1, 1, 250, 0, 3),
 (2, 1, 250, 0, 3),
@@ -238,15 +238,13 @@ insert  into `pedido_produto`(`idPedido_Produto`,`quantidade`,`valor`,`Pedido_id
 
 
 /*Data for the table `endereco` */
-
-insert into endereco (`id_endereco`, `numero_endereco`, `rua`, `bairro`, `cidade`, `estado`, `cep`, `complemento`) VALUES
+insert into `endereco` (`id_endereco`, `numero_endereco`, `rua`, `bairro`, `cidade`, `estado`, `cep`, `complemento`) VALUES
 (1, 90, `Beco Pamplona`, `7 de Setembro`, `Gaspar`, `Santa Catarina`, `89114-876`, 501),
 (2, 00, `Rodovia sc-414`, `Vila Nova`, `Luiz Alves`, `Santa Catarina`, `89128-000`, 0),
 (3, 8267, `Rodovia sc-414`, `Vila Nova`, `Luiz Alves`, `Santa Catarina`, `89128-000`, 0);
 
 
 /*Data for the table `cliente`*/
-
 insert  into `cliente` (`id_cliente`, `nome`, `CPF`) values (1,`Ferdinando Rainert`, `119.082.619-43`);
 insert into `cliente` (`id_cliente`, `nome`, `CPF`) values (2,`Bruno Erbs`, `108.912.169-52`);
 insert into `cliente` (`id_cliente`, `nome`, `CPF`) values (3,`Rafael Antônio Bressanini`, `103.725.339-65`);
@@ -254,11 +252,21 @@ insert into `cliente` (`id_cliente`, `nome`, `CPF`) values (4,`Thiago Leopoldo B
 
 
 /*Data for the table `vendedor`*/
-
 insert into `vendedor` (`id_vendedor`, `nome`, `CPF`, `senha`) values (1,`Ferdinando Rainert`, `119.082.619-43`, `Porradesenha24@`);
 insert into `vendedor` (`id_vendedor`, `nome`, `CPF`, `senha`) values (2, `Bruno Erbs`,`108.912.169-52`, `erbs1311`);
 insert into `vendedor` (`id_vendedor`, `nome`, `CPF`, `senha`) values (3, `Rafael Antõnio Bressanini`, `103.725.339-65`, `123456`);
 insert into `vendedor` (`id_vendedor`, `nome`, `CPF`, `senha`) values (4, `Thiago Leopoldo Beffart Weber`, `076.147.659-82`, `123456`);
+
+
+/*Data for the table `telefone` */
+
+
+
+/*Data for the table `email` */
+
+
+
+/*Data for the table `caracteristicas` */
 
 
 
