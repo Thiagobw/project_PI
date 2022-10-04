baad100c6ba5d9e184954e6c6c0cbd7c9c2fc786
RafaelBressanini <laimportsloja@gmail.com>
2022-10-03 22:40:58 -0300

    delete_usuario

---
+++ DAO/delete.php
@@ -0,0 +1,12 @@
+<?php
+
+include_once 'connection.php';
+
+$id_usuario = filter_input(INPUT_GET, 'id_usuario', FILTER_SANITIZE_SPECIAL_CHARS);
+$queryDelete = $link->query("DELETE FROM usuarios WHERE id_usuario='$id_usuario'");
+
+if(mysqli_affected_rows($link) > 0){
+    header("location:../")// botar a localização da pagina de colsulta 
+
+}
+?>
