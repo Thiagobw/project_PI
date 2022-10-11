f7c0d8f6203b24e1bedd4b62bf513edb4f331563
thiagoBW <thgleopoldo900@gmail.com>
2022-10-11 11:36:51 -0300

    nd

--- view/dashboard/sidebar.php
+++ view/dashboard/sidebar.php
@@ -1,30 +1,30 @@
 <div class="sidebar pe-4 pb-3">
-            <nav class="navbar bg-secondary navbar-dark">
-                <a href="./" class="navbar-brand mx-4 mb-3">
-                    <h3 class="text-primary"><i class="fa fa-user-edit me-2"></i>Dashboard</h3>
-                </a>
-                <div class="navbar-nav w-100">
-                    <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle active mb-1" data-bs-toggle="dropdown">
-                            <div class="d-flex align-items-center ms-1 mb-1 mt-1" style="align-items: center;">
-                                <div class="position-relative">
-                                    <img class="rounded-circle" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
-                                    <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
-                                </div>
-                                <div class="ms-3">
-                                    <h6 class="mb-0"><?php echo $shortName; ?></h6>
-                                    <span>Admin</span>
-                                </div>
-                            </div>
-                        </a>
-                        <div class="dropdown-menu bg-transparent border-0">
-                            <a href="" class="dropdown-item">Perfil</a>
-                            <a href="../../control/sair.php" class="dropdown-item">Sair</a>
+    <nav class="navbar bg-secondary navbar-dark">
+        <a href="./" class="navbar-brand mx-4 mb-3">
+            <h3 class="text-primary"><i class="fa fa-user-edit me-2"></i>Dashboard</h3>
+        </a>
+        <div class="navbar-nav w-100">
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle active mb-1" data-bs-toggle="dropdown">
+                    <div class="d-flex align-items-center ms-1 mb-1 mt-1" style="align-items: center;">
+                        <div class="position-relative">
+                            <img class="rounded-circle" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                            <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
+                        </div>
+                        <div class="ms-3">
+                            <h6 class="mb-0"><?php echo $shortName; ?></h6>
+                            <span>Admin</span>
                         </div>
                     </div>
-                    <a href="index.php" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Painel</a>
-                    <a href="customers.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Clientes</a>
-                    <a href="products.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Produtos</a>
+                </a>
+                <div class="dropdown-menu bg-transparent border-0">
+                    <a href="" class="dropdown-item">Perfil</a>
+                    <a href="../../control/sair.php" class="dropdown-item">Sair</a>
                 </div>
-            </nav>
+            </div>
+            <a href="index.php" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Painel</a>
+            <a href="customers.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Clientes</a>
+            <a href="products.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Produtos</a>
         </div>
+    </nav>
+</div>
