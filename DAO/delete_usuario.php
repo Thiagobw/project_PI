f9dbe4da9580b8899685f06529e88bc7e4a7cee0
bruno <brunoerbs4@gmail.com>
2022-10-13 08:05:57 -0300

    correção de erro

--- DAO/delete_usuario.php
+++ DAO/delete_usuario.php
@@ -6,7 +6,7 @@ $id_usuario = filter_input(INPUT_GET, 'id_usuario', FILTER_SANITIZE_SPECIAL_CHAR
 $queryDelete = $link->query("DELETE FROM usuarios WHERE id_usuario='$id_usuario'");
 
 if(mysqli_affected_rows($link) > 0){
-    header("location:../");// botar a localização da pagina de colsulta 
+    header("location:../");// botar a localização da pagina de consulta 
 
 }
 ?>
