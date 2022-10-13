026c657c6917e520c002527cbaa43dbba8447a89
thiagobw <thgleopoldo900@gmail.com>
2022-10-13 09:08:51 -0300

    interface cadastro de clientes

--- view/dashboard/register-prod-cli.php
+++ view/dashboard/register-prod-cli.php
@@ -1,18 +1,36 @@
 <div class="modal-dialog">
     <div class="modal fade" id="PopUp-register-cli-prod" aria-hidden="true" tabindex="-1">
-        <div class="modal-dialog modal-fullscreen">
+        <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalToggleLabel">Modal 1</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <h5 class="modal-title" id="exampleModalToggleLabel">Cadastrar Cliente</h5>
+                    <button type="button" class="btn btn-danger btn-close" data-bs-dismiss="modal" aria-label="Close">
+                    
+                    </button>
                 </div>
     
                 <div class="modal-body">
-                  teste
-                </div>
-    
-                <div class="modal-footer">
-    
+                    <div class="row" id="account">
+                        <div class="col-12 input-group">
+                            <form class="input-group center" id="form1">
+                                <div class="col-12 col-sm-10 col-md-9 col-lg-8 col-xl-8 mt-3">
+                                    <input class=" form-control input-user" type="text" placeholder="Nome" id="nameCli" maxlength="100" name="nomeCli">
+                                </div>
+
+                                <div class="col-12 col-sm-10 col-md-9 col-lg-8 col-xl-8 mt-3">
+                                    <input class=" form-control input-user" type="text" placeholder="CPF" id="cpfCli"  name="cpfCli" maxlength="14" autocomplete="off">
+                                </div>
+
+                                <div class="col-12 col-sm-10 col-md-9 col-lg-8 col-xl-8 mt-3">
+                                    <input class=" form-control input-user" type="email" placeholder="Email" id="emailCli"  name="emailCli">
+                                </div>
+
+                                <div class="col-8 col-sm-5 col-lg-5 mt-3 center">
+                                    <input class="btn btn-primary w-100 btn-log" form="form1" type="submit" name="log-in" value="Cadastrar Cliente">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
