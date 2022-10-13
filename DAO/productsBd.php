a2fb3ea236051a23f7ed76999ff94d1d10592925
ferdinando <ferdinando.r1@aluno.ifsc.edu.br>
2022-10-13 09:50:53 -0300

    produto com banco

---
+++ DAO/productsBd.php
@@ -0,0 +1,27 @@
+<?php
+include_once "../../DAO/connection.php";
+include_once "../../model/Products.php";
+
+
+function buscar_produto(){
+    $conexao = connect();
+
+    $stmt = $conexao->prepare("SELECT * FROM produtos");
+    $stmt->execute();
+
+    $result = $stmt->fetchAll();
+    $resul_produtos = array();
+
+    foreach($result as $registro){
+        $produto = new Products();
+        $produto->setCodigo($registro["id_produtos"]);
+        $produto->setName($registro["nome_produto"]);
+        $produto->setAmount($registro["quantidade"]);
+        $produto->setPrice($registro["preco_produto"]);
+
+        $resul_produtos[] = $produto;
+    }
+
+
+    return $resul_produtos;
+    }
