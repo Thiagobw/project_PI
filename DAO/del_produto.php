cfd82a8d4553aa9529984f641f65c7d09453ac4d
thiagoBW <thgleopoldo900@gmail.com>
2022-10-21 23:39:55 -0300

    correção de arquivos desnecessarios

--- DAO/del_produto.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$id_produto = filter_input(INPUT_GET, 'id_produto', FILTER_SANITIZE_NUMBER_INT);
-
-echo 'Quer mesmo excluir esse produto? ';
-echo "<a href='delete.php? id_produto= $id_produto> sim </a>'";
-echo "<a href='index.php'>não</a>";
-?>
