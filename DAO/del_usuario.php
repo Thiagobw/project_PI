89e7e3f54e516be245c5677ca0d77b02a8ad96c2
RafaelBressanini <laimportsloja@gmail.com>
2022-10-26 11:25:23 -0300

    deletando-impecilios

--- DAO/del_usuario.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$id_usuario = filter_input(INPUT_GET, 'id_usuario', FILTER_SANITIZE_NUMBER_INT);
-
-echo 'Quer mesmo excluir esse registro? ';
-echo "<a href='delete.php? id_usuario= $id_usuarioid> sim </a>'";
-echo "<a href='index.php'>não</a>";
-?>
--- DAO/del_venda.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$id_pedido = filter_input(INPUT_GET, 'id_pedido', FILTER_SANITIZE_NUMBER_INT);
-
-echo 'Quer mesmo excluir esse registro? ';
-echo "<a href='delete.php? id_pedido= $id_pedido> sim </a>'";
-echo "<a href='index.php'>não</a>";
-?>
