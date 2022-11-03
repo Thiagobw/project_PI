2d611e00c26707a168bb838bbf895b63af40edd2
thiagobw <thgleopoldo900@gmail.com>
2022-11-03 10:24:56 -0300

    importação jquery atualizado

--- view/dashboard/customersPage.php
+++ view/dashboard/customersPage.php
@@ -212,7 +212,7 @@ $customersList = search_customers();
     </div>
 
     <!-- JavaScript Libraries -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="lib/chart/chart.min.js"></script>
     <script src="lib/easing/easing.min.js"></script>
