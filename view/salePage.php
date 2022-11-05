f54100d1d3e132c15b763e15dee8fb49df3a2095
Thiagobw <96150000+Thiagobw@users.noreply.github.com>
2022-11-05 03:04:58 -0300

    correções

--- view/salePage.php
+++ view/salePage.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-include_once "../control/checkAuth.php";
-include_once "../DAO/productsBd.php";
+include_once $_SERVER["DOCUMENT_ROOT"]."/project_PI/control/checkAuth.php";
+include_once $_SERVER["DOCUMENT_ROOT"]."/project_PI/DAO/productsBd.php";
 
 ?>
 <!DOCTYPE html>
@@ -15,7 +15,7 @@ include_once "../DAO/productsBd.php";
 
     <link rel="stylesheet" href="css/styleSalePage.css">
 
-    <title>LA Imports - Vender produto</title>
+    <title>LA Imports - Vender</title>
 </head>
 <body>
 
@@ -25,158 +25,38 @@ include_once "../DAO/productsBd.php";
     ?>
 </header>
 
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-12">
-            <!-- space -->
-        </div>
-    </div>
-</div>
-
-<!-- modal - product information -->
-<div id="modalInformation" class="modal fade">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-
-            <div class="modal-header">
-                <h2 class="modal-title">Informações do produto</h2>
-                <button type="button" class="btn btn-close" data-bs-dismiss="modal" aria-label="Close"><img class="img-fluid" src="img/icons/Icon_close.png" alt="" height="22" width="22"></button>
-            </div>
-
-            <div class="modal-body">
-                <div class="row center">
-                    <div class="col-7">
-                        <img class="img-fluid w-100" src="img/products/tenis 1.jpg" alt="">
-                    </div>
-                </div>
-                <div class="row center">
-                <div class="col-8">
-                        <h3 class="text-center mt-2" contenteditable="true">Nike Air Jordan High 1 Tie Dye</h3>
-                        <p class="text-center mt-2">Codigo: <span>N4D0L0B0B0J</span></p>
-                    </div>
-                </div>
-
-                <div class="row mt-1" style="border-top: 0.05px solid #e9ecef;">
-                    <div class="col-12 mt-3">
-                        <p class="price">R$ <span contenteditable="true">400,00</span></p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="modal-footer"></div>
-        </div>
-    </div>
-</div>
-
-
 <!-- products -->
 <main class="mt-1">
     <div class="container-fluid center">
         <div class="col-12 col-sm-11 col-md-11 col-lg-10 col-xl-11">
-        <div class="row justify-content-around">
+            <div class="row justify-content-around">
 
-            <div class="col-11 col-sm-6 col-md-6 col-lg-4 col-xl-3 bg-prod mt-4">
-                <div class="row">
-                    <div class="col-12 col-sm-11 col-xl-10 bg-white prod">
-                        <div class="row center mb-2 mt-2">
-                            <img class="img-fluid img" src="img/products/tenis 1.jpg" alt="">
-                        </div>
-
-                        <div class="col-12 dividing-line"></div>
-
-                        <div class="row mt-3">
-                            <div class="col-12 ">
-                                <p class="text-center">Nike Sb Dunk Low</p>
-                                <p class="text-center price">R$ <span>499,00</span></p>
+                <div class="col-11 col-sm-6 col-md-6 col-lg-4 col-xl-3 col-xxl-2 bg-prod">
+                    <div class="row">
+                        <div class="col-12 col-sm-11 col-xl-10 bg-white prod mt-4">
+                            <div class="row center mb-2 mt-2">
+                                <img class="img-fluid img" src="img/products/tenis 1.jpg" alt="">
                             </div>
-                        </div>
-                        <div class="row center mb-2">
-                            <div class="col-12 btn-group center">
-                                <button class="btn btnADD" onclick="pedido_produto('Nike Sb Dunk Low')"><span><img class="img-fluid" src="img/icons/icon_AddShoppingCart.png" alt="" height='27' width='27'></span> Adicionar ao carrinho</button>
+
+                            <div class="col-12 dividing-line"></div>
+
+                            <div class="row mt-3">
+                                <div class="col-12 ">
+
+                                    <p class="text-center">Nike Air Jordan High 1 Tie Dye - unissex</p>
+                                    <p class="text-center price">R$ <span>400,00</span></p>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="col-11 col-sm-6 col-md-6 col-lg-4 col-xl-3 bg-prod mt-4">
-                <div class="row">
-                    <div class="col-12 col-sm-11 col-xl-10 bg-white prod">
-                        <div class="row center mb-2 mt-2">
-                            <img class="img-fluid img" src="img/products/tenis 1.jpg" alt="">
-                        </div>
-
-                        <div class="col-12 dividing-line"></div>
-
-                        <div class="row mt-3">
-                            <div class="col-12 ">
-                                <p class="text-center">Balenciaga Triple S</p>
-                                <p class="text-center price">R$ <span>250,00</span></p>
-                            </div>
-                        </div>
-                        <div class="row center mb-2">
-                            <div class="col-12 btn-group center">
-                                <button class="btn btnADD" onclick="pedido_produto('Balenciaga Triple S')"><span><img class="img-fluid" src="img/icons/icon_AddShoppingCart.png" alt="" height='27' width='27'></span> Adicionar ao carrinho</button>
+                            <div class="row center mb-2">
+                                <div class="col-12 btn-group center">
+                                    <button class="btn btnADD" onclick="pedido_produto('Nike Air Jordan High 1 Tie Dye - unissex')"><span><img class="img-fluid" src="img/icons/icon_AddShoppingCart.png" alt="" height='27' width='27'></span> Adicionar ao carrinho</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
 
-
-            <div class="col-11 col-sm-6 col-md-6 col-lg-4 col-xl-3 col-xxl-2 bg-prod">
-                <div class="row">
-                    <div class="col-12 col-sm-11 col-xl-10 bg-white prod mt-4">
-                        <div class="row center mb-2 mt-2">
-                            <img class="img-fluid img" src="img/products/tenis 1.jpg" alt="">
-                        </div>
-
-                        <div class="col-12 dividing-line"></div>
-
-                        <div class="row mt-3">
-                            <div class="col-12 ">
-                                <p class="text-center">Adidas Yezzy Boost 350 V2</p>
-                                <p class="text-center price">R$ <span>350,00</span></p>
-                            </div>
-                        </div>
-                        <div class="row center mb-2">
-                            <div class="col-12 btn-group center">
-                                <button class="btn btnADD" onclick="pedido_produto('Adidas Yezzy Boost 350 V2')"><span><img class="img-fluid" src="img/icons/icon_AddShoppingCart.png" alt="" height='27' width='27'></span> Adicionar ao carrinho</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="col-11 col-sm-6 col-md-6 col-lg-4 col-xl-3 col-xxl-2 bg-prod">
-                <div class="row">
-                    <div class="col-12 col-sm-11 col-xl-10 bg-white prod mt-4">
-                        <div class="row center mb-2 mt-2">
-                            <img class="img-fluid img" src="img/products/tenis 1.jpg" alt="">
-                        </div>
-
-                        <div class="col-12 dividing-line"></div>
-
-                        <div class="row mt-3">
-                            <div class="col-12 ">
-
-                                <p class="text-center">Nike Air Jordan High 1 Tie Dye - unissex</p>
-                                <p class="text-center price">R$ <span>400,00</span></p>
-                            </div>
-                        </div>
-                        <div class="row center mb-2">
-                            <div class="col-12 btn-group center">
-                                <button class="btn btnADD" onclick="pedido_produto('Nike Air Jordan High 1 Tie Dye - unissex')"><span><img class="img-fluid" src="img/icons/icon_AddShoppingCart.png" alt="" height='27' width='27'></span> Adicionar ao carrinho</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-            
         </div>
     </div>
 </main>
