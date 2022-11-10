6fe4d89c53b85802f793e49eb89e177a1411d385
RafaelBressanini] <laimportsloja@gmail.com>
2022-11-10 11:26:52 -0300

    ajuste

--- control/products_registration.php
+++ control/products_registration.php
@@ -24,3 +24,31 @@ if ($result_regist == true) {
 else {
     echo "falha ao cadastrar";
 }
+
+<?php
+
+$imagem = "imagem.jpg"; //aqui a url da imagem que sera criada a miniatura
+
+header("Content-type: image/jpeg"); // Cabeçalho do Script para informar o tipo da imagem lembrando que se for gerar uma imagem ig vc tem que mudar o cabeçalho isso serve para qualquer tipo de imagem
+
+$im = imagecreatefromjpeg($imagem); //criar uma amostra da imagem original
+
+$largurao = imagesx($im); // pegar a largura da amostra
+
+$alturao = imagesy($im); // pegar a altura da amostra
+
+$alturad = 100; // definir a altura da miniatura em px
+
+$largurad = ($largurao*$alturad)/$alturao; // calcula a largura da imagem a partir da altura da miniatura
+
+$nova = imagecreatetruecolor($largurad,$alturad); //criar uma imagem em branco
+
+imagecopyresampled($nova,$im,0,0,0,0,$largurad,$alturad,$largurao,$alturao); //copiar sobre a imagem em branco a amostra diminuindo conforma as especificações da miniatura
+
+imagejpeg($nova); //cria imagem jpeg
+
+imagedestroy($nova); //libera a memoria usada na miniatura
+
+imagedestroy($im); //libera a memoria usada na amostra
+
+?>
