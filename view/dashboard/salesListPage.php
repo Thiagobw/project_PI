f0835b0ccb0958d4bcca1493bb5d2b6f1e52ad68
Thiagobw <96150000+Thiagobw@users.noreply.github.com>
2022-11-26 01:16:09 -0300

    alterações

---
+++ view/dashboard/salesListPage.php
@@ -0,0 +1,199 @@
+<?php
+@session_start();
+include_once $_SERVER["DOCUMENT_ROOT"]."/project_PI/control/checkAuth.php";
+include_once $_SERVER["DOCUMENT_ROOT"]."/project_PI/DAO/Productsbd.php";
+
+$productsList = search_products();
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <title>LA Imports - lista de vendas</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Roboto:wght@500;700&display=swap"> 
+    
+    <!-- Icon Font Stylesheet -->
+    <link rel="stylesheet" href="../img/icons/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+
+    <!-- Libraries Stylesheet -->
+    <link rel="stylesheet" href="lib/owlcarousel/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    
+
+    <!-- Template Stylesheet -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container-fluid position-relative d-flex p-0">
+        <!-- Spinner Start -->
+        <div id="spinner" class="show bg-dark position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        <!-- Spinner End -->
+
+
+        <!-- Sidebar Start -->
+        <?php
+            require_once "sidebar.php";
+        ?>
+        <!-- Sidebar End -->
+
+
+        <!-- Content Start -->
+        <div class="content">
+            <!-- Navbar Start -->
+            <nav class="navbar navbar-expand bg-secondary navbar-dark sticky-top px-4 py-0">
+                <a href="#" class="navbar-brand d-flex d-lg-none me-4">
+                    <h2 class="text-primary mb-0"><i class="fa fa-user-edit"></i></h2>
+                </a>
+
+                <a href="#" class="sidebar-toggler flex-shrink-0 mt-3 mb-2">
+                    <i class="fa fa-bars"></i>
+                </a>
+            </nav>
+            <!-- Navbar End -->
+
+            <!-- Importing popup file -->
+            <?php
+                require_once "popUps/popUp-register.php";
+                include_once "popUps/popUp-change.php";
+            ?>
+            <!-- Table Start -->
+            <div class="container-fluid pt-4 px-4">
+                <div class="row g-4">
+                    
+                    <div class="col-12">
+                        <div class="bg-secondary rounded h-100 p-4">
+                            <div class="row top-table">
+                                <div class="col-12 col-xl-12" style="padding: 0;">
+                                    <div class="col-12 col-xl-10 mb-3">
+                                        <h6 class=" ml-1 text-center text-white" style="font-size: larger;">Lista de Vendas</h6>
+                                    </div>
+                                    <div class="col-1 btn-register mb-2">
+                                        <button type="button" id="btnRegisterProd" data-bs-toggle="modal" data-bs-target="#PopUp_register"><i class="fa-solid fa-gift"></i> +</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="table-responsive">
+                                <table class="table text-secondary">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Nome do produto</th>
+                                            <th scope="col">estoque</th>
+                                            <th scope="col">Preço</th>
+                                            <th scope="col">Tamanhos</th>
+                                            <th scope="col">Ações</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    <?php 
+                                        if (  empty($productsList) == true) {
+                                    ?>
+                                    
+                                    <tr>
+                                        <td class="text-white text-center" colspan="6">Nenhum produto cadastrado!</td>
+                                    </tr>
+
+                                    <?php 
+                                    
+                                    
+                                    } else{
+
+                                     foreach ($productsList as $prod) {
+   
+                                    ?>
+                                        <tr>
+                                            <td> <?php echo $prod -> getName(); ?></td>
+                                            <td> fazer aqui!! </td>
+                                            <td> <?php echo "R$ ".$prod -> getPrice().",00";?> </td>
+                                            <td> fazer aqui!! </td>
+                                            <td>
+                                                <div class="btn-group" role="group" aria-label="Basic example">
+                                                    <a class="btn btn-sm btn-plus-action" id="btnAlterProd" data-bs-toggle="modal" data-bs-target="#PopUp_alter" href="" onclick="getProductData(<?php echo $prod -> getId(); ?>)">alterar</a>
+                                                    <a class="btn btn-sm btn-plus-action" href="../../control/products_delete.php?id=<?php echo $prod ->getId(); ?>">Excluir</a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                     
+                                     <?php }} ?>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Table End -->
+            
+        </div>
+        <!-- Content End -->
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/chart/chart.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/tempusdominus/js/moment.min.js"></script>
+    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+
+    <script>
+            var btn = document.querySelector('#btnRegisterProd');
+
+            btn.addEventListener('click', function() {
+
+            const contentR = document.querySelector('#contentRegisterProd');
+            contentR.style.display = 'flex';
+
+            const ttlR = document.querySelector('#ttl-products');
+            ttlR.style.display = 'flex';
+        })
+    </script>
+
+    <script>
+        let imgselected = document.querySelector('#imgSelected');
+        let inputImage = document.querySelector('#imgProduct');
+        var imgSelect = document.querySelector('#imgSelect');
+        inputImage.addEventListener('click', () => {
+            inputImage.click();
+        });
+        inputImage.addEventListener('change', (e) => {
+            
+        let reader = new FileReader();
+        reader.onload = () => {
+            imgselected.src = reader.result;
+            imgSelect.style.display = 'none';
+            imgselected.style.display = 'flex';
+        }
+        reader.readAsDataURL(inputImage.files[0]);
+        })
+    </script>
+</body>
+</html>
