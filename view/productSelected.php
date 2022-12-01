e6be9ca81ba3e5520fd83e8efff08242ba8dd4dd
Thiagobw <96150000+Thiagobw@users.noreply.github.com>
2022-12-01 07:28:13 -0300

    estilização

--- view/productSelected.php
+++ view/productSelected.php
@@ -2,7 +2,7 @@
 include_once '../DAO/cartBd.php';
 include_once '../DAO/productsBd.php';
 include_once '../DAO/imagesBd.php';
-session_start();
+@session_start();
 function checkAuth()
 {
   if (isset($_SESSION['autenticado']) && $_SESSION['autenticado'] == true) {
@@ -34,101 +34,131 @@ $img = selectImage($prod->getImagemId()); //get the img object->name
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/bootstrap.css">
   <link rel="stylesheet" href="css/styleHomePage.css">
-  <!-- <link rel="stylesheet" href="css/styleSelected.css"> -->
   <link rel="stylesheet" href="img/icons/fontawesome/css/all.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 
-  <title>Escolha os Tamanhos - LA Imports</title>
+  <title>LA Imports - Escolher Tamanhos</title>
 </head>
 
-<body>
+<style>
+  .img-prod {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .text-prod h6 {
+    font-size: larger;
+    font-weight: 500;
+  }
+  .text-prod p {
+    font-size: medium;
+    font-weight: 300;
+  }
+  @media (min-width: 240px) and (max-width: 575px) {
+    .text-prod {
+      text-align: center;
+    }
+  }
+  
+</style>
+
+<body class="bg-secondary">
 
   <header>
     <?php
     include "../view/navbar/navbarHomePage.php";
-    include "../view/pop-upAccount.php";
     ?>
   </header>
-  <section class="h-100 h-custom" style="background-color: #eee;">
-    <div class="container py-5 h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col">
-          <div class="card">
-            <div class="card-body p-4">
 
+  <section class="container">
+    <div class="row center">
+      <div class="col-12 bg-white mt-5 rounded-bottom shadow-lg">
+
+        <div class="card-body">
+          
+            <form class="col-12" id="productSelected" method="POST" action="/project_PI/control/control_cart.php">
+
+              <!-- Product information -->
               <div class="row">
-                <form class="col-12 cart-body" id="productSelected" method="POST" action="/project_PI/control/control_cart.php">
-                  <!-- product product in cart -->
-                  <div class="cart-item">
-                    <!-- Product information -->
-                    <div class="row cart-row">
-                      <div class="col-12 col-sm-2 pic">
-                        <span>
-                          <!-- product image, his geting the name of file intended on uploads folder -->
-                          <img class="img-fluid" src="../uploads/<?php echo $img->getName() ?>" alt="">
-                        </span>
-                      </div>
-                      <!-- name product -->
-                      <div class="col-12 col-sm-10 desc">
-                        <h6><?php echo $prod->getName() //using object example
-                            ?></h6>
-                        <p>Selecione o tamanho</p>
-                        <?php //echo var_dump($sizes) << debug 
-                        ?>
-                        <!-- available sizes -->
-                        <?php foreach ($sizes as $size) {
-                          if ($size['quantidade'] < $_SESSION['quantity']) //see out-of-stock
-                          { ?>
-                            <label class="col-3 mt-3">
-                              <?php echo $size['tamanho']; ?>
-                              <p class="small mr-2 invalid">
-                                Sem estoque
-                                Temos:<?php echo $size['quantidade'] ?>
-                              </p>
-                            </label>
-                          <?php } else { ?>
-                            <label class="col-3 mt-3">
-                              <input type="checkbox" name="sizeSelected[]" value="<?php echo $size['tamanho'] //this to send correctly id by "auto_increment" id of database 
-                                                                                  ?>">
-                              <?php echo ':' . $size['tamanho']; ?>
-                              <p class="small flex mr-1">
-                                Em estoque:<?php echo $size['quantidade'] ?>
-                              </p>
-                            </label>
-                          <?php } ?>
-                        <?php
-                        } ?>
-                      </div>
+                <div class="col-12 col-sm-2 img-prod">
+                    <!-- product image, his geting the name of file intended on uploads folder -->
+                    <img class="img-fluid shadow rounded" src="../uploads/<?php echo $img->getName() ?>" alt="">
+                </div>
 
-                      <!-- price product selected -->
-                      <div class="cart-row-cell amount">
-                        <p text class="fa fa-text-big">R$ <span><?php echo $prod->getPrice() * $_SESSION['quantity']; ?> cada</span></p>
+                <!-- name and price product -->
+                <div class="col-12 col-sm-8 text-prod">
+                  <h6 class="mt-3"> <?php echo $prod->getName();?> </h6>
+                  <p class="mt-2">R$ <?php echo $prod->getPrice() * $_SESSION['quantity'].',00'; ?> cada unidade</p>
+                </div>
+                <!-- name and price product -->
+              </div>
+              <!-- Product information -->
+
+
+              <div class="row mt-5">
+                <div class="col-12 center">
+                  <p>Selecione o tamanho desejado</p>
+                </div>
+                
+                <!-- available sizes -->
+                <div class="col-12">
+                  <div class="row center">
+                  <?php foreach ($sizes as $size) {
+                    if ($size['quantidade'] < $_SESSION['quantity']) { //see out-of-stock
+                    ?>
+                    
+                      <!-- message if no stock -->
+                      <label class="col-11 col-sm-5 col-md-3 col-lg-3 col-xl-3 mt-2">
+                        <div class="alert alert-danger" role="alert">
+                          <?php echo $size['tamanho']; ?>
+                          <p class="small">Sem estoque!</p>
+                        </div>
+                      </label>
+                      <!-- message if no stock -->
+
+                    <?php } else { ?>
+
+
+                      <label class="col-11 col-sm-6 col-md-4 col-lg-3 col-xl-3 mt-2">
+                      <div class="alert alert-success" role="alert">
+                        <input type="checkbox" name="sizeSelected[]" value="<?php echo $size['tamanho'];?>"><!-- this to send correctly id by "auto_increment" id of database -->
+                        <?php echo $size['tamanho']; ?>
+                        <p class="small flex mr-1">
+                          Em estoque:<?php echo $size['quantidade'] ?>
+                        </p>
                       </div>
-                    </div>
+                      </label>
+                    <?php } ?>
+                  <?php
+                  } ?>
                   </div>
+                </div>
+                <!-- price product selected -->
+                <div class="cart-row-cell amount">
+                  
+                </div>
+              </div>
 
-                  <div class="col-12">
-                    <div class="row center">
+            <div class="col-12">
+              <div class="row center">
 
-                      <!-- button to cancel selection -->
-                      <div class="col-12 col-sm-4 center mt-3 mb-1">
-                        <a href="salePage.php" class="btn btn-danger">Cancelar</a>
-                      </div>
+                <!-- button to cancel selection -->
+                <div class="col-12 col-sm-4 center mt-3 mb-1">
+                  <a href="salePage.php" class="btn btn-danger">Cancelar</a>
+                </div>
 
-                      <!-- button to confirm selection -->
-                      <div class="col-12 col-sm-4 center mt-2 mb-1">
-                        <input type="submit" class="btn btn-success" value="Confirmar">
-                      </div>
-                    </div>
-                  </div>
-
-                </form>
+                <!-- button to confirm selection -->
+                <div class="col-12 col-sm-4 center mt-2 mb-1">
+                  <input type="submit" class="btn btn-success" value="Confirmar">
+                </div>
               </div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
+    </div>
   </section>
+
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
