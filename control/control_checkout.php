25b20291c4f07a78d79e42a7c689a963e1d74502
Thiagobw <96150000+Thiagobw@users.noreply.github.com>
2022-12-06 02:39:43 -0300

    venda concertada

--- control/control_checkout.php
+++ control/control_checkout.php
@@ -3,10 +3,37 @@ include_once $_SERVER["DOCUMENT_ROOT"]."/project_PI/DAO/requestsBd.php";
 @session_start();
 //see you come through cart button!
 
-if (isset($_GET['checkout'])) {
+if (isset($_GET['checkout']) || isset($_GET['makeOrder'])) {
 
-    die("deu bom");
+    if (isset($_GET['checkout'])) {
+        header("Location: /project_PI/view/checkout.php");
+    }
+    
+    if (isset($_GET['makeOrder'])) {
+        
+        
+        if(isset($_GET['method'])) {
 
+            if (!empty(getSelectedMethod())) {
+                $_SESSION['method_selected'] = getSelectedMethod();
+
+
+                if(makeOrder($_SESSION['id_usuario'], $_SESSION['method_selected'], $_GET['subtotal'], $_GET['employe'], $_GET['customer'])) {
+            
+                    return header("Location: /project_PI/view/dashboard/salesListPage.php");
+                }else{
+                    return header("Location: /project_PI/view/cart.php");
+                }
+
+            } else {
+                header("Location: /project_PI/view/checkout.php");
+            }
+        }
+        
+        else {
+            header("Location: /project_PI/view/checkout.php");
+        }
+    }
 
 
 } else {
@@ -14,37 +41,26 @@ if (isset($_GET['checkout'])) {
 }
 
 
+function getSelectedMethod ():string {
 
+    if ($_GET['method'] == "tB") {
+        return "Transf bancária";
 
+    } elseif ($_GET['method'] == "blt") {
+        return "Boleto";
 
-if(isset($_GET['makeOrder'])) {
-    
-    
-    if(isset($_GET['method'])) {
-        $_SESSION['method_selected'] = 'Transf bancária';
-    }else {
-        // return header("Location: /project_PI/view/checkout.php");
+    } elseif ($_GET['method'] == "pix") {
+        return "Pix";
+
+    }elseif ($_GET['method'] == "card") {
+        return "Cartão";
+
+    } elseif ($_GET['method'] == "money") {
+        return "Dinheiro";
+
+    } else {
+        return "";
     }
-
-    
-
-    if(makeOrder($_SESSION['id_usuario'], $_SESSION['method_selected'], $_GET['subtotal'], $_GET['employe'],
-    $_GET['customer'])) {
-
-        return header("Location: /project_PI/view/dashboard/");
-    }else{
-        echo "n deu"; //return header("Location: /project_PI/view/cart.php");
-    }
-    
-    if(makeOrder($_SESSION['id_usuario'], $_SESSION['method_selected'], $_GET['subtotal'], $_GET['employe'], $_GET['customer']))
-    {
-        return header("Location: /project_PI/view/dashboard/");
-    }else {
-        return header("Location: /project_PI/view/cart.php");
-    }
-
-}else {
-    header("Location: /project_PI/view/Error404.html");
 }
 
 ?>
